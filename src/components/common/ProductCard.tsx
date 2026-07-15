3fec43474cd2b729a90bbc97eff711dc9bb80e5f
szymon <szymon.wos1319@gmail.com>
2026-07-15 14:11:15 +0200

    fix - mock products img url

--- src/components/common/ProductCard.tsx
+++ src/components/common/ProductCard.tsx
@@ -127,7 +127,7 @@ const ProductCard = ({
             )}
             {product.images?.data ? (
               <Image
-                src={`${process.env.API_URL}/mock-data/mock-images/${product.images.data[0].attributes.url}`}
+                src={`/mock-data/mock-images/${product.images.data[0].attributes.url}`}
                 alt={product.name!}
                 fill
                 style={{ objectFit: 'cover' }}
